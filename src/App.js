e930ffef79f45e8d3ad6758908c2b000117681f3
Tarik <tarikmerrylees@gmail.com>
2020-12-25 22:28:30 +0000

    using mapStateToProps and created Score component folder

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import './App.css';
-// import Counter from './components/Counter';
+import Score from "./components/Score";
 
 const App = ({team_score, onIncrement, onDecrement}) => ( 
-<>
-    <h1>Set Score</h1>
-    
-    <p>{ team_score }</p>
+    <>
+        <h1>Set Score</h1>
+        
+        <Score team_score = { team_score } />
 
-    <div>
-        <button
-            onClick= { onIncrement }
-        >
-            +
-        </button>
+        <div>
+            <button
+                onClick= { onIncrement }
+            >
+                +
+            </button>
 
 
-        <button
-            onClick= { onDecrement }
-        >
-            -
-        </button>
-    </div>
-</>
+            <button
+                onClick= { onDecrement }
+            >
+                -
+            </button>
+        </div>
+    </>
 );
 
 export default App;
---
+++ src/components/Score/Score.js
@@ -0,0 +1,7 @@
+const Score = ({ team_score }) => ( 
+    <>  
+        <p>{ team_score }</p>
+    </>
+);
+
+export default Score;
