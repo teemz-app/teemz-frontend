e4a241d60c379a6033d73ee9d6bfadd65b788611
Tarik <tarikmerrylees@gmail.com>
2020-12-26 19:48:15 +0000

    added other initial states for later use

--- src/data/Initial.js
+++ src/data/Initial.js
@@ -3,7 +3,11 @@ const initial = {
     team_one_score: 0,
     team_two_score: 0,
 
+    team_one_name: "",
+    team_two_name: "",
+
     all_players: [],
+    ten_players: [],
     team_one_players: ["Katara", "Ang", "Apa", "Sokka", "Momo"],
     team_two_players: ["Pabu", "Iroh", "Azula", "Zuko", "Toph"],
 
