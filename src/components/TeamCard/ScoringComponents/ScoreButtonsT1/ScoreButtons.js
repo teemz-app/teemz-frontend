354661f49130a206a90dcf3229bc86b8e7dca4a4
Tarik <tarikmerrylees@gmail.com>
2020-12-28 17:36:39 +0000

    moved plus and buttons around

--- src/components/TeamCard/ScoringComponents/ScoreButtonsT1/ScoreButtons.js
+++ src/components/TeamCard/ScoringComponents/ScoreButtonsT1/ScoreButtons.js
@@ -3,17 +3,16 @@ const ScoreButtons = ({ onIncrement, onDecrement}) => (
         <div>
             <button
                 className="teamCard_setScoreButton"
-                onClick= { onIncrement }
-            >
-                +
-            </button>
-
-
-            <button
-                className="teamCard_setScoreButton"
                 onClick= { onDecrement }
             >
                 -
+            </button>
+
+            <button
+                className="teamCard_setScoreButton"
+                onClick= { onIncrement }
+            >
+                +
             </button>
         </div>
     </>
--- src/components/TeamCard/ScoringComponents/ScoreButtonsT2/ScoreButtons.js
+++ src/components/TeamCard/ScoringComponents/ScoreButtonsT2/ScoreButtons.js
@@ -3,18 +3,18 @@ const ScoreButtons = ({ onIncrement, onDecrement}) => (
         <div>
             <button
                 className="teamCard_setScoreButton"
+                onClick= { onDecrement }
+            >
+                -
+            </button>
+
+            <button
+                className="teamCard_setScoreButton"
                 onClick= { onIncrement }
             >
                 +
             </button>
 
-
-            <button
-                className="teamCard_setScoreButton"
-                onClick= { onDecrement }
-            >
-                -
-            </button>
         </div>
     </>
 );
