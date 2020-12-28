f88768240717ce5cebe400b3369bcf6eff41a839
Tarik <tarikmerrylees@gmail.com>
2020-12-28 17:30:18 +0000

    MVP styling finisshed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ const App = () => (
         <header>
             <h1 className="title">Teemz</h1>
         </header>
-        <GenerateMatch />
+        
         <GenerateTeams />
+        <GenerateMatch />
     </>
 );
 
