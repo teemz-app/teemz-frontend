228e2b601cf52f2a5263c1de1bc19705b364d683
Tarik <tarikmerrylees@gmail.com>
2020-12-29 08:25:44 +0000

    axios installed

--- src/data/store.js
+++ src/data/store.js
@@ -5,7 +5,7 @@ import { createStore } from "redux";
 
 const store = createStore(reducer,
     initial,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 export default store;
