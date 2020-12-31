a303096b37ae5429e60d53bab15504e93a5a3a94
Tarik <tarikmerrylees@gmail.com>
2020-12-31 16:27:54 +0000

    teambalancing and team generation logic completed.

--- src/components/GeneratePage/GeneratePage.js
+++ src/components/GeneratePage/GeneratePage.js
@@ -3,7 +3,7 @@ import GeneratingMatch from '../../pages/GeneratingMatch';
 import Button from "../Button";
 import { useSelector, useDispatch } from 'react-redux';
 
-// Fisher yates shuffle to shuffle players array into random order
+// Fisher yates shuffle to shuffle players array into random order, and is considered an efficient way of shuffling an array.
 let fisherYatesShuffle = (players) => {
     for (let i = players.length - 1; i > 0; i -= 1) {
         let j = Math.floor(Math.random() * (i + 1));
@@ -13,6 +13,7 @@ let fisherYatesShuffle = (players) => {
     return players;
 }
 
+// We're shuffling the players in the array and then taking all the players needed from the beginning of the array. Now we have the players needed to fill two teams.
 let selectedPlayers = (teams_size, players) => {
     let allPLayers = [...fisherYatesShuffle(players)];
     let playerPool = [];
@@ -21,24 +22,52 @@ let selectedPlayers = (teams_size, players) => {
         playerPool.push(allPLayers[i]);
     }
 
-    console.log(playerPool);
-    return playerPool;
+    return playerPool.sort(compareByWR);
 }
 
-let setTeamMembers = (teamOnePlayers, teamTwoPlayers, playerPool) => {
-        // teamOnePlayers.length >= teamTwoPlayers.length ? teamTwoPlayers.push(playerPool[i]) : teamOnePlayers.push(playerPool[i]);
-    // let playersToPlay = [...playerPool];
-    // let playerWR = ({player}) => player.wins/(player.wins + player.losses);
+// This allows us to calculate the win rate of individual players, which will be used for 1) reordering the array of players to join the teams in order of win rate and 2) help us determine which player goes into which team
+let playerWR = (player) => player.wins === 0 ? -(player.losses) : player.wins / (player.wins + player.losses);
 
+// The re-ordering logic required inside .sort() for reordering the players in terms of win rate
+let compareByWR = (player1, player2) => {
+    let player1WR = playerWR(player1);
+    let player2WR = playerWR(player2);
 
-    for (let i = 0; i < playerPool.length; i += 1) {
+    if (player1WR < player2WR) {
+        return 1;
+    } else if (player1WR > player2WR) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
 
+// Splitting the players into two teams
+/*
+Team 1 get the odd numbered indexes, and Team 2 will get the even numbered indexes
+*/
+let setTeamMembers = (playerPool) => {
+    let firstOfT1 = playerPool[0]; // 1st player odd 
+    let firstOfT2 = playerPool[1]; // 2nd player even
+
+    let team_one_players = []; // t1[0, 3, 5, 7, 9]
+    team_one_players.push(firstOfT1);  // t2[1, 2, 4, 6, 8]
+    console.log("Team one: ", team_one_players);
+    let team_two_players = [];
+    team_two_players.push(firstOfT2);
+    console.log("Team two: ", team_two_players);
+
+    for (let i = 2; i < (playerPool.length); i += 1) {
+        i % 2 === 0 ? team_two_players.push(playerPool[i]) : team_one_players.push(playerPool[i]);
     }
 
-    return {
-        team_one_players: teamOnePlayers,
-        team_two_players: teamTwoPlayers,
+    let output  = {
+        team_one_players: team_one_players,
+        team_two_players: team_two_players,
     };
+
+    console.log(output);
+    return output;
 }
 
 let GeneratePage = () => {
@@ -47,11 +76,8 @@ let GeneratePage = () => {
     let players = [...useSelector(state => state.players)];
     let teams_size = useSelector(state => state.teams_size);
 
-    let teamOnePlayers = useSelector(state => state.team_one_players);
-    let teamTwoPlayers = useSelector(state => state.team_two_players);
-
     let playerPool = selectedPlayers(teams_size, players);
-    setTeamMembers(teams_size, teamOnePlayers, teamTwoPlayers, playerPool);
+    setTeamMembers(playerPool);
 
 
     return (
@@ -60,8 +86,9 @@ let GeneratePage = () => {
             <GeneratingMatch />
 
             <Button
-            onClick={ console.log("saved!") }
+            onClick={ console.log("stop logging!") }
             label="Save Game!"
+            type="button"
             />
         </>
     )
