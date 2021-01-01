6162bd1a199b5fae2acbb47c9cd75d96c2a2eaee
Tarik <tarikmerrylees@gmail.com>
2021-01-01 19:41:27 +0000

    moved generate team logic into seperate directory

--- src/data/actions/api.js
+++ src/data/actions/api.js
@@ -1,5 +1,10 @@
-// import axios from '../../data/axios-config';
+import axios from '../../data/axios-config';
 // import { addPlayers } from './state';
 
 export const getPlayers = () => {
+    return (dispatch) => {
+        axios.get('players').then(({ data }) => {
+            dispatch({ type: "GET_PLAYERS", data: data.data });
+        });
+    }
 };
