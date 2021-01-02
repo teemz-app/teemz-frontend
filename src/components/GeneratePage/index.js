7f66ccaa0a5f3e1bfb04a208da5cccedb082abc8
Tarik <tarikmerrylees@gmail.com>
2021-01-02 21:18:31 +0000

    updating players when saving games

--- src/components/GeneratePage/index.js
+++ src/components/GeneratePage/index.js
@@ -2,13 +2,15 @@ import { connect } from 'react-redux';
 import GeneratePage from './GeneratePage';
 import { postMatch } from '../../data/actions/api';
 
-const mapStatetoProps = ({ team_one_name, team_one_score, team_two_name, team_two_score }) => {
+const mapStatetoProps = (state) => {
     return {
         data: {
-            team_one_name: team_one_name,
-            team_one_score: team_one_score,
-            team_two_name: team_two_name,
-            team_two_score: team_two_score,
+            team_one_name: state.team_one_name,
+            team_one_score: state.team_one_score,
+            team_two_name: state.team_two_name,
+            team_two_score: state.team_two_score,
+            team_one_players: state.team_one_players,
+            team_two_players: state.team_two_players,
         }
     };
 }
