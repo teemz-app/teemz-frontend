b440303448401997ee60973b13c4a9de914b3fd5
Tarik <tarikmerrylees@gmail.com>
2021-01-03 17:12:24 +0000

    player added styled

--- src/components/PlayerAdded/PlayerAdded.js
+++ src/components/PlayerAdded/PlayerAdded.js
@@ -1,15 +1,12 @@
 const PlayerAdded = ({ playersAdded }) => {
     if (playersAdded > 0) {
-
-        for (let i = 0; i < playersAdded; i += 1) {
-            return (
-                <>  
-                    <p className="teamCard_teamScore">Player Added!</p>
-                </>
-            )
-        }
+        return (
+            <>  
+                <p className="playerAdded">Player Added!</p>
+            </>
+        )
     }
-    return;
+    return <></>;
 };
 
 export default PlayerAdded;
